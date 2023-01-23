fc2fd9c2fe467a6aad226af6e8be04a1d62f448d
Gustavo Oliveira <gustaoliveiradev@gmail.com>
2023-01-22 23:34:56 -0400

    primeiroInput

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page>
+
+    <p>Formulário Quasar</p>
+
+    <q-input
+     rounded outlined
+     v-model="text"
+    label="Digite seu Nome"
+    
+    />
+    
+  </q-page>
+</template>
+
+
+
+
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
